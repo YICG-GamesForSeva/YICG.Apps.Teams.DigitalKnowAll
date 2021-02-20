7882b70e76a466fe01d02f864b0e1bec51983fe4
pkcodes92 <pkrish19@outlook.com>
2021-02-20 15:17:18 -0500

    Adding the TicketValidationException, KnowledgeBaseSettings and QnAMakerSettings class files

---
+++ Source/YICG.Apps.Teams.DigitalKnowBot/YICG.Apps.Teams.DigitalKnowBot.Common/Models/Configuration/KnowledgeBaseSettings.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="KnowledgeBaseSettings.cs" company="Games For Seva">
+// Copyright (c) Games For Seva. All rights reserved.
+// </copyright>
+
+namespace YICG.Apps.Teams.DigitalKnowBot.Common.Models.Configuration
+{
+    /// <summary>
+    /// This class represents the knowledge base settings.
+    /// </summary>
+    public class KnowledgeBaseSettings
+    {
+        /// <summary>
+        /// Gets or sets the storage connection string.
+        /// </summary>
+        public string StorageConnectionString { get; set; }
+
+        /// <summary>
+        /// Gets or sets the search service name.
+        /// </summary>
+        public string SearchServiceName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the search service query api key.
+        /// </summary>
+        public string SearchServiceQueryApiKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the search service admin api key.
+        /// </summary>
+        public string SearchServiceAdminApiKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the search indexing interval in minutes.
+        /// </summary>
+        public string SearchIndexingIntervalInMinutes { get; set; }
+    }
+}
