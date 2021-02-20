4564bcd61babae68154339c66fc290b39905f29d
pkcodes92 <pkrish19@outlook.com>
2021-02-20 16:28:30 -0500

    Adding additional models before the necessary code gets developed

---
+++ Source/YICG.Apps.Teams.DigitalKnowBot/YICG.Apps.Teams.DigitalKnowBot.Common/Models/TicketSearchScope.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="TicketSearchScope.cs" company="Games For Seva">
+// Copyright (c) Games For Seva. All rights reserved.
+// </copyright>
+
+namespace YICG.Apps.Teams.DigitalKnowBot.Common.Models
+{
+    /// <summary>
+    /// This enumeration represents the ticket search scope.
+    /// </summary>
+    public enum TicketSearchScope
+    {
+        /// <summary>
+        /// Recent tickets
+        /// </summary>
+        RecentTickets,
+
+        /// <summary>
+        /// Open tickets
+        /// </summary>
+        OpenTickets,
+
+        /// <summary>
+        /// Tickets assigned to a subject-matter expert
+        /// </summary>
+        AssignedTickets,
+    }
+}
