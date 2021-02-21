370311534169c0d5b1ce521dc01714987cb662ea
pkcodes92 <pkrish19@outlook.com>
2021-02-21 16:08:42 -0500

    Adding the Ticket related classes and enumerations

---
+++ Source/YICG.Apps.Teams.DigitalKnowBot/YICG.Apps.Teams.DigitalKnowBot.Common/Models/TicketEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YICG.Apps.Teams.DigitalKnowBot.Common.Models
+{
+    class TicketEntity
+    {
+    }
+}
---
+++ Source/YICG.Apps.Teams.DigitalKnowBot/YICG.Apps.Teams.DigitalKnowBot.Common/Models/TicketState.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="TicketState.cs" company="Games For Seva">
+// Copyright (c) Games For Seva. All rights reserved.
+// </copyright>
+
+namespace YICG.Apps.Teams.DigitalKnowBot.Common.Models
+{
+    /// <summary>
+    /// Represents the current state/status of a ticket.
+    /// </summary>
+    public enum TicketState
+    {
+        /// <summary>
+        /// Represents an active ticket
+        /// </summary>
+        Open = 0,
+
+        /// <summary>
+        /// Represents a ticket that requires no further action
+        /// </summary>
+        Closed = 1,
+
+        /// <summary>
+        /// Sentinel value
+        /// </summary>
+        MaxValue = Closed,
+    }
+}
