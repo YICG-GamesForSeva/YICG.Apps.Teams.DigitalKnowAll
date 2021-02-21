174628837a7a55e49a5a1bf60db2b93992587018
pkcodes92 <pkrish19@outlook.com>
2021-02-21 15:39:21 -0500

    Ensuring to have the necessary models inserted

---
+++ Source/YICG.Apps.Teams.DigitalKnowBot/YICG.Apps.Teams.DigitalKnowBot.Common/Models/FeedbackRating.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="FeedbackRating.cs" company="Games For Seva">
+// Copyright (c) Games For Seva. All rights reserved.
+// </copyright>
+
+namespace YICG.Apps.Teams.DigitalKnowBot.Common.Models
+{
+    /// <summary>
+    /// Represents the user rating given for the feedback.
+    /// </summary>
+    public enum FeedbackRating
+    {
+        /// <summary>
+        /// Not helpful rating
+        /// </summary>
+        NotHelpful,
+
+        /// <summary>
+        /// Needs improvement rating
+        /// </summary>
+        NeedsImprovement,
+
+        /// <summary>
+        /// Helpful rating
+        /// </summary>
+        Helpful,
+    }
+}
