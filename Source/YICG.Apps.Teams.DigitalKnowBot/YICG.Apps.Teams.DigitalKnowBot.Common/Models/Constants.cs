c1bdbafa14f08cd1e3a0c1bd21dd1dbccc020c4f
Pranav Krishnamurthy <74801920+pkcodes92@users.noreply.github.com>
2021-03-13 18:19:08 -0500

    Having the skeleton created to be able to put in the necessary cards correctly (#5)
    * Pause point for march 7th
    
    * Completing the skeleton for personal welcome message, and take a tour functionality to be done

---
+++ Source/YICG.Apps.Teams.DigitalKnowBot/YICG.Apps.Teams.DigitalKnowBot.Common/Models/Constants.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="Constants.cs" company="Games For Seva">
+// Copyright (c) Games For Seva. All rights reserved.
+// </copyright>
+
+namespace YICG.Apps.Teams.DigitalKnowBot.Common.Models
+{
+    /// <summary>
+    /// This class defines all the constants that the Ganga Game Bot is required to use.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// This constant represents a conversation in the personal scope (1:1 with the bot).
+        /// </summary>
+        public const string ConversationTypePersonal = "personal";
+
+        /// <summary>
+        /// This constant represents a conversation in a Team (many users + bot).
+        /// </summary>
+        public const string ConversationTypeChannel = "channel";
+
+        /// <summary>
+        /// This constant represents the command to be taking a tour in the personal scope.
+        /// </summary>
+        public const string TakeATourPersonalCommand = "take a tour";
+
+        /// <summary>
+        /// This constant represents the command to ask an expert in the personal scope.
+        /// </summary>
+        public const string AskAnExpertPersonalCommand = "ask an expert";
+
+        /// <summary>
+        /// This constant represents the command to share feedback in the personal scope.
+        /// </summary>
+        public const string ShareFeedbackPersonalCommand = "share feedback";
+
+        /// <summary>
+        /// This constant represents the command to ask an expert from a card.
+        /// </summary>
+        public const string AskAnExpertFromCard = "ask an expert";
+
+        /// <summary>
+        /// This constant represents the command to share feedback from a card.
+        /// </summary>
+        public const string ShareFeedbackFromCard = "share feedback";
+
+        /// <summary>
+        /// This constant represents the command to take a tour in the team scope.
+        /// </summary>
+        public const string TeamTourChannelCommand = "team tour";
+    }
+}
--- Source/YICG.Apps.Teams.DigitalKnowBot/YICG.Apps.Teams.DigitalKnowBot/Bots/GangaGameBot.cs
+++ Source/YICG.Apps.Teams.DigitalKnowBot/YICG.Apps.Teams.DigitalKnowBot/Bots/GangaGameBot.cs
@@ -1,4 +1,4 @@
-// <copyright file="GangaGameBot.cs" company="Games For Seva">
+﻿// <copyright file="GangaGameBot.cs" company="Games For Seva">
 // Copyright (c) Games For Seva. All rights reserved.
 // </copyright>
 
@@ -10,15 +10,40 @@ namespace YICG.Apps.Teams.DigitalKnowBot.Bots
     using System.Threading.Tasks;
     using Microsoft.Bot.Builder;
     using Microsoft.Bot.Schema;
+    using YICG.Apps.Teams.DigitalKnowBot.Common.Models;
 
     /// <summary>
     /// This class is our main bot class that will execute all of the functionality.
     /// </summary>
     public class GangaGameBot : ActivityHandler
     {
-        private const string ConversationTypePersonal = "personal";
+        /// <summary>
+        /// This method always executes whenever a message is sent to the bot, or a message reaction is posted.
+        /// </summary>
+        /// <param name="turnContext">The current turn in the conversation.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A unit of execution.</returns>
+        public override Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            // Conduct a null check on the turnContext - do we have some data in the conversation turn?
+            if (turnContext is null)
+            {
+                throw new ArgumentNullException(nameof(turnContext));
+            }
 
-        private const string ConversationTypeChannel = "channel";
+            switch (turnContext.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    return this.OnMessageActivityAsync(new DelegatingTurnContext<IMessageActivity>(turnContext), cancellationToken);
+                case ActivityTypes.ConversationUpdate:
+                    return this.OnConversationUpdateActivityAsync(new DelegatingTurnContext<IConversationUpdateActivity>(turnContext), cancellationToken);
+                default:
+                    turnContext.SendActivityAsync(MessageFactory.Text("I have no clue what you are saying, please try again!"), cancellationToken);
+                    break;
+            }
+
+            return base.OnTurnAsync(turnContext, cancellationToken);
+        }
 
         /// <summary>
         /// This method executes whenever there is a new message coming into the bot.
@@ -33,8 +58,28 @@ namespace YICG.Apps.Teams.DigitalKnowBot.Bots
                 throw new ArgumentNullException(nameof(turnContext));
             }
 
-            var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            try
+            {
+                var message = turnContext.Activity;
+                await this.SendTypingIndicatorAsync(turnContext);
+
+                switch (message.Conversation.ConversationType)
+                {
+                    case Constants.ConversationTypePersonal:
+                        await this.OnMessageActivityInPersonalChatAsync(message, turnContext, cancellationToken);
+                        break;
+                    case Constants.ConversationTypeChannel:
+                        await this.OnMessageActivityInChannelAsync(message, turnContext, cancellationToken);
+                        break;
+                    default:
+                        await turnContext.SendActivityAsync(MessageFactory.Text($"I cannot understand the {message.Conversation.ConversationType} conversation!"), cancellationToken);
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         /// <summary>
@@ -63,6 +108,77 @@ namespace YICG.Apps.Teams.DigitalKnowBot.Bots
                 {
                     await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
                 }
+            }
+        }
+
+        private async Task SendTypingIndicatorAsync(ITurnContext turnContext)
+        {
+            try
+            {
+                var typingActivity = turnContext.Activity.CreateReply();
+                typingActivity.Type = ActivityTypes.Typing;
+                await turnContext.SendActivityAsync(typingActivity);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private async Task OnMessageActivityInPersonalChatAsync(IMessageActivity message, ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (message is null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            if (turnContext is null)
+            {
+                throw new ArgumentNullException(nameof(turnContext));
+            }
+
+            string messageText = (message.Text ?? string.Empty).Trim().ToLower();
+
+            switch (messageText)
+            {
+                case Constants.TakeATourPersonalCommand:
+                    await turnContext.SendActivityAsync(MessageFactory.Text("Hang on! I'm working on the tour, will give it soon!"), cancellationToken);
+                    break;
+                case Constants.AskAnExpertPersonalCommand:
+                    await turnContext.SendActivityAsync(MessageFactory.Text("Pump the breaks! You need help already?! I'll get it to you!"), cancellationToken);
+                    break;
+                case Constants.ShareFeedbackPersonalCommand:
+                    await turnContext.SendActivityAsync(MessageFactory.Text("You're telling on me?! Really?! How rude!! 👀"), cancellationToken);
+                    break;
+                default:
+                    var replyText = $"Echo: {messageText}";
+                    await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+                    break;
+            }
+        }
+
+        private async Task OnMessageActivityInChannelAsync(IMessageActivity message, ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (message is null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            if (turnContext is null)
+            {
+                throw new ArgumentNullException(nameof(turnContext));
+            }
+
+            string text = (message.Text ?? string.Empty).Trim().ToLower();
+
+            switch (text)
+            {
+                case Constants.TeamTourChannelCommand:
+                    await turnContext.SendActivityAsync(MessageFactory.Text("Cool, you want to know how I help team! I'll show you in a minute!"), cancellationToken);
+                    break;
+                default:
+                    await turnContext.SendActivityAsync(MessageFactory.Text("Ooook... My 🤖 🧠 cannot understand!!!"), cancellationToken);
+                    break;
             }
         }
     }
