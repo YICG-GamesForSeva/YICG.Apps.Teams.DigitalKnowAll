702b8a8c96671bd404a20264d9793a2effed8d60
pkcodes92 <pkrish19@outlook.com>
2021-04-17 18:33:37 -0400

    Adding various changes as below: - Qna Maker full integration is done - ResponseCard - part 1 (rendering the QnA Maker response) is done

---
+++ Source/YICG.Apps.Teams.DigitalKnowBot/YICG.Apps.Teams.DigitalKnowBot/Cards/ResponseCard.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="ResponseCard.cs" company="Games For Seva">
+// Copyright (c) Games For Seva. All rights reserved.
+// </copyright>
+
+namespace YICG.Apps.Teams.DigitalKnowBot.Cards
+{
+    using System.Collections.Generic;
+    using AdaptiveCards;
+    using Microsoft.Bot.Schema;
+    using YICG.Apps.Teams.DigitalKnowBot.Properties;
+
+    /// <summary>
+    /// This class is responsible for rendering a QnA Maker response if found for a user question.
+    /// </summary>
+    public static class ResponseCard
+    {
+        /// <summary>
+        /// This method will construct a card, and render the QnA Maker answer in a user friendly way.
+        /// </summary>
+        /// <param name="question">The Question from the QnA Maker.</param>
+        /// <param name="answer">The Answer from the QnA Maker.</param>
+        /// <param name="userQuestion">The question that the user has asked the bot.</param>
+        /// <returns>An attachment to append to a message.</returns>
+        public static Attachment GetCard(string question, string answer, string userQuestion)
+        {
+            AdaptiveCard responseCard = new AdaptiveCard("1.0")
+            {
+                Body = new List<AdaptiveElement>
+                {
+                    new AdaptiveTextBlock
+                    {
+                        Weight = AdaptiveTextWeight.Bolder,
+                        Text = Strings.ResponseCardHeaderText,
+                        Wrap = true,
+                    },
+                    new AdaptiveTextBlock
+                    {
+                        Text = answer,
+                        Wrap = true,
+                    },
+                },
+            };
+
+            return new Attachment
+            {
+                Content = responseCard,
+                ContentType = AdaptiveCard.ContentType,
+            };
+        }
+    }
+}
